8176b18dd7cc942955df004f28c2c4bd54dd9adb
Dang Tran Hoang Ha <80314055+hoangha-2711@users.noreply.github.com>
2023-10-14 19:23:23 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public String condition(int h, int t)
 {
     if (h >= 170 && h <= 190) 
     {
-        if (18 <= t && t <= 21) 
+        if ( t >= 18 && t <= 21) 
         {
             return "valid";
         }
